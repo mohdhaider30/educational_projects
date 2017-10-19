4d3cf082eae1fb6aa46fb4485d51c4b1b4c8eda7
tbhaxor <tbhaxor@gmail.com>
2017-10-19 06:56:07 +0530

    csharp_stumanage

---
+++ STUDENT_MANAGEMENT_IN_CSHARP_MYSQL/Student_Management_I/SQL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Student_Management_I
+{
+    class SQL
+    {
+        string host, user, database, password;
+        MySqlConnection conn;
+        public SQL(string host,string user,string password,string database)
+        {
+            this.host = host;
+            this.user = user;
+            this.database = database;
+            this.password = password;
+            conn = new MySqlConnection("SERVER = ;DATABASE =;UID=;PASSWORD=;");
+        }
+        public bool connect()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public bool dispose()
+        {
+            try
+            {
+                conn.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
